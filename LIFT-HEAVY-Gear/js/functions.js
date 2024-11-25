37b116efee467b966b9a315e4b24570869faaa1d
Francesco Barzanti <francesco.barzanti@studio.unibo.it>
2024-11-25 22:35:08 +0100

    make menu, edit files.add styleClass.css

---
+++ LIFT-HEAVY-Gear/js/functions.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    menuToggle.addEventListener('click', (e) => {
+        e.preventDefault();
+        dropdownMenu.classList.toggle('hidden');
+    });
+});
