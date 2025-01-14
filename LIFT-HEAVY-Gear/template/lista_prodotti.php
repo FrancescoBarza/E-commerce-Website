7c58e05241499fb798ee77036376177e51ad20fc
Francesco Barzanti <francesco.barzanti@studio.unibo.it>
2025-01-14 14:54:38 +0100

    accessibilità immagini

--- LIFT-HEAVY-Gear/template/lista_prodotti.php
+++ LIFT-HEAVY-Gear/template/lista_prodotti.php
@@ -1,9 +1,9 @@
 <div class="slider">
             <button class="prev" onclick="prevImage()">❮</button>
             <div class="image-container">
-                <img src="images/uploads/home_sheffield.png" alt="Immagine 1" class="active">
-                <img src="images/uploads/home_sheffield2025.png" alt="Immagine 2">
-                <img src="images/uploads/home_deadlift.png" alt="Immagine 3">
+                <img src="images/uploads/home_sheffield.png" alt="Palco Sheffield 2024" class="active">
+                <img src="images/uploads/home_sheffield2025.png" alt="Sheffield 2025">
+                <img src="images/uploads/home_deadlift.png" alt="Stacco da terra">
             </div>
             <button class="next" onclick="nextImage()">❯</button>
         </div>
@@ -12,7 +12,7 @@
             <?php foreach($templateParams["lista-prodotti"] as $prodotto ): ?>
             <div class="prodotto">
                 <a href="prodotto.php?id=<?php echo $prodotto["ID_prodotto"];?>">
-                    <img src="<?php echo UPLOAD_DIR_UPLOADS.$prodotto["immagine"]; ?>" alt="Immagine prodotto" />
+                    <img src="<?php echo UPLOAD_DIR_UPLOADS.$prodotto["immagine"]; ?>" alt="<?php echo $prodotto["nome"];?>" />
                     <h3><?php echo $prodotto["nome"]; ?></h3>
                     <p><?php echo $prodotto["prezzo"]; ?> €</p>
                 </a>
