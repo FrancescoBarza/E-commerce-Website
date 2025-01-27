60d6b968a098b68ebb2e8e87f8bfc24d118bfabe
Luca Latini <l.latini03@gmail.com>
2025-01-27 16:54:57 +0100

    p

--- LIFT-HEAVY-Gear/template/scheda_prodotto.php
+++ LIFT-HEAVY-Gear/template/scheda_prodotto.php
@@ -6,14 +6,16 @@
             <p><?php echo $prodotto["prezzo"]; ?> €</p><br>
             <h3>Descrizione:</h3>
             <p><?php echo $prodotto["descrizione"]; ?></p>
-            <form action="processa-form-carrello.php" method="POST" enctype="multipart/form-data">
-                <input type="hidden" name="idprodotto" value="<?php echo $prodotto["ID_prodotto"]; ?>">
-                <input type="hidden" name="action" value="7">
-                <input type="hidden" name="quantita" value="1"> <!-- Quantità di default -->
-        
-                <button type="submit"><span class="fas fa-cart-plus"></span></button>
-            </form>
+            
+            <?php if (!isset($_SESSION["venditore"]) || $_SESSION["venditore"] !== 'Y'): ?>
+               
+                <form action="processa-form-carrello.php" method="POST" enctype="multipart/form-data">
+                    <input type="hidden" name="idprodotto" value="<?php echo $prodotto["ID_prodotto"]; ?>">
+                    <input type="hidden" name="action" value="7">
+                    <input type="hidden" name="quantita" value="1"> <!-- Quantità di default -->
+                    <button type="submit"><span class="fas fa-cart-plus"></span> Aggiungi al carrello</button>
+                </form>
+            <?php endif; ?>
         <?php endforeach; ?>
-
     </div>
 </section>
